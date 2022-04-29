7182d8243f83dd167b7477ac6d02089027b87012
채명정 <jack2718@naver.com>
2022-04-29 17:56:13 +0900

    feat(design):Facade Pattern

---
+++ src/main/java/com/example/portfolio_demo/global/design/facade/FTP.java
@@ -0,0 +1,28 @@
+package com.example.portfolio_demo.global.design.facade;
+
+public class FTP {
+
+    private String host;
+
+    private int port;
+
+    private String path;
+
+    public FTP(String host, int port, String path){
+        this.host = host;
+        this.port = port;
+        this.path = path;
+    }
+
+    public void connect(){
+        System.out.println("FTP Host : " + host + "Port :" + port + "로 연결 합니다.");
+    }
+
+    public void moveDirectory(){
+        System.out.println("path :" + path + "로 이동 합니다.");
+    }
+
+    public void disConnect(){
+        System.out.println("FTP 연결을 종료 합니다.");
+    }
+}
