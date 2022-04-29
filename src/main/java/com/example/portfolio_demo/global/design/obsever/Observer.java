e513b13fa0f46268e8cf3eea4c150fae66d14d74
채명정 <jack2718@naver.com>
2022-04-29 14:06:06 +0900

    feat(design):Observer Pattern

---
+++ src/main/java/com/example/portfolio_demo/global/design/obsever/Observer.java
@@ -0,0 +1,9 @@
+package com.example.portfolio_demo.global.design.obsever;
+
+/*
+관찰자 패턴은 변화가 일어 났을 때, 미리 등록된 다른 클래스에 토옵해주는 패턴을 구현한 것이다.
+많이 보이는 곳은 event listener 에서 해당 패턴을 사용 하고 있다.
+*/
+public class Observer {
+
+}
