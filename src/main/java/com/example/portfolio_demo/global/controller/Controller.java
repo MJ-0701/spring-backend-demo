ce70f134165de66a991117579f8687a562fccb43
채명정 <jack2718@naver.com>
2022-05-02 22:58:20 +0900

    feat(api):POST API

--- src/main/java/com/example/portfolio_demo/global/controller/Controller.java
+++ src/main/java/com/example/portfolio_demo/global/controller/Controller.java
@@ -14,6 +14,7 @@ public class Controller {
         return "test"; // 성공
     }
 
+    // GET API
     @GetMapping("/get/path-variable/{name}") // http://localhost:8080/api/get/path-variable/{name}
     public String pathVariable(@PathVariable(name = "name") String pathName){ // 기본적으로 파라미터와 경로변수의 이름은 같아야 함. 하지만 어쩔수 없는 경우 따로 (name = "name") 으로 맞춰 줄 수 있음
         System.out.println("PathVariable : " + pathName);
@@ -52,6 +53,66 @@ public class Controller {
         return userRequestDTO.toString(); // UserRequestDTO(name=채명정, email=jack2718@naver.com, age=32)
     }
 
+    // POST API
+/*
+웹에서 데이터를 주고 받을때 xml 방식과 JSON 방식이 있는데 현재는 JSON 방식만 사용한다고 보면 된다.
 
+JSON 형태는
+string : value
+number : value
+boolean : value
+object : value {}
+array : value []
+
+JSON을 사용하는 규칙은 snake_case 와 kamelCase 가 있는데 둘다 파싱이 가능 하여야 하며 통상적으로 snake_case 를 사용한다.
+
+{
+    "phone_number" : "010-1111-2222", -> String
+    "phoneNumber" : "010-1111-2222",
+    "age" : 10, -> number
+    "isAgree" : false, -> boolean
+    "account" : { -> object
+            "email" : "jack2718@naver.com",
+            "password" : "1234",
+            "address" : "서울 특별시"
+    },
+    "user_list" : [ -> array
+            {
+                "account" : "abcd",
+                "password" : "1234"
+            },
+            {
+                "account" : "efgh",
+                "password" : "1234"
+            },
+            {
+                "account" : "ijkln",
+                "password" : "1234"
+            },
+
+    ]
+}
+*/
+
+/*
+{
+    "account" : "",
+    "email" : "",
+    "password" : "",
+    "address" : ""
+}
+*/
+    @PostMapping("/post")
+    public void post(@RequestBody Map<String,Object> requestData){
+        requestData.forEach((key, value) -> {
+            System.out.println("key :" + key);
+            System.out.println("value :" + value);
+        });
+    }
+
+    @PostMapping("/post01")
+    public void post(@RequestBody UserRequestDTO userRequestDTO){ // 실제 사용 방법. POST 일때 @RequestBody 어노 테이션을 사용해야 데이터를 파싱할 수 있다.
+        System.out.println(userRequestDTO);
+    }
 
 }
--- src/main/java/com/example/portfolio_demo/global/domain/dto/req/UserRequestDTO.java
+++ src/main/java/com/example/portfolio_demo/global/domain/dto/req/UserRequestDTO.java
@@ -1,13 +1,26 @@
 package com.example.portfolio_demo.global.domain.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class UserRequestDTO {
+
+    private String account;
+
+    private String address;
+
+    private String password;
 
     private String name;
 
     private String email;
 
     private int age;
+
+    @JsonProperty("phone_number") // JSON 에서 snake_case 로 받아오게 되면 매칭이 안되므로 지정을 해줘야됨.
+    private String phoneNumber;
+
+    @JsonProperty("OTP")
+    private String OTP;
 }
